753c7940f879dd7310f79518c76c96efd0b31330
huanmingchang <huanming1988@gmail.com>
2022-04-26 21:09:15 +0800

    feat: finish ScreenPreview components and styles

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <v-app>
     <Navbar />
+    <v-main>
+      <v-container fluid>
+        <v-row>
+          <v-col> <ScreenPreview /> </v-col>
+          <v-col> <v-card>right side</v-card> </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
     <Footer />
   </v-app>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
-
+import ScreenPreview from './components/ScreenPreview.vue'
 import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components: {
     Navbar,
+    ScreenPreview,
     Footer,
   },
 }
---
+++ src/components/ScreenPreview.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card>
+    <v-card-title>Screen Preview</v-card-title>
+    <v-card-text class="preview rounded"></v-card-text>
+    <v-card-actions>
+      <v-text-field
+        label="Text"
+        hide-details="auto"
+        class="mr-16"
+        color="blue-grey darken-2"
+      ></v-text-field>
+      <v-btn color="blue-grey darken-2" text> Add Text</v-btn>
+      <v-btn color="cyan darken-3" text> Take photo </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ScreenPreview',
+}
+</script>
+
+<style lang="scss" scoped>
+.preview {
+  border: 1px solid #455a64;
+  height: 500px;
+}
+</style>
