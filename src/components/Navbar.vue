ed1ed6903c05bbff4eb702c0038fd8118a3c8814
huanmingchang <huanming1988@gmail.com>
2022-04-26 20:21:10 +0800

    styles: adjust navbar style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,15 @@
 <template>
   <v-card class="overflow-hidden">
-    <v-app-bar
-      absolute
-      color="white"
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-    >
+    <v-app-bar absolute color="white">
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
       <v-toolbar-title>Capture Webcam App</v-toolbar-title>
-
       <v-spacer></v-spacer>
-
       <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-7"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1500px"> </v-container>
+    <v-sheet id="scrolling-techniques-7">
+      <v-container style="height: 60px"> </v-container>
     </v-sheet>
   </v-card>
 </template>
