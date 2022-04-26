3c411af253906df11b57900569271bc4f3d91011
huanmingchang <huanming1988@gmail.com>
2022-04-26 20:08:15 +0800

    feat: project init

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer dark padless>
+    <v-card-text class="py-2 white--text text-center">
+      {{ new Date().getFullYear() }} — <strong>&copy; Anson</strong>
+    </v-card-text>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
