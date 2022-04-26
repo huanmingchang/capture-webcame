ec935b2b214dbe28ab8070e4b6f52300bb50c318
huanmingchang <huanming1988@gmail.com>
2022-04-26 21:22:12 +0800

    feat: add SnapShots component and styles

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <v-container fluid>
         <v-row>
           <v-col> <CameraPreview /> </v-col>
-          <v-col> <v-card>right side</v-card> </v-col>
+          <v-col> <SnapShots /> </v-col>
         </v-row>
       </v-container>
     </v-main>
@@ -17,12 +17,14 @@
 <script>
 import Navbar from './components/Navbar.vue'
 import CameraPreview from './components/CameraPreview.vue'
+import SnapShots from './components/SnapShots.vue'
 import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components: {
     Navbar,
     CameraPreview,
+    SnapShots,
     Footer,
   },
 }
